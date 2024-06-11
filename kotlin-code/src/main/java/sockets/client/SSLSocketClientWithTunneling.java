5d9024ae0be850d2721871c5de3f2d976af92b92
byteflys <easingforever2000@gmail.com>
2024-06-11 22:48:31 +0800

    create project

--- kotlin-code/src/main/java/sockets/client/SSLSocketClientWithTunneling.java
+++ kotlin-code/src/main/java/sockets/client/SSLSocketClientWithTunneling.java
@@ -161,7 +161,7 @@ public class SSLSocketClientWithTunneling {
         OutputStream out = tunnel.getOutputStream();
         String msg = "CONNECT " + host + ":" + port + " HTTP/1.0\n"
                      + "User-Agent: "
-                     + sun.net.www.protocol.http.HttpURLConnection.userAgent
+                     + "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
                      + "\r\n\r\n";
         byte b[];
         try {
