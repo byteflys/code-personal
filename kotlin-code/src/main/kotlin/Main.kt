0098d18f166d2b3f6375f20451abb71e9abe7d96
easing <easing@onyx-international.com>
2024-06-15 17:28:41 +0800

    rxjava timeout

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 import io.reactivex.rxjava3.core.Observable
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 fun main() {
     Observable.just(0)
         .map {
             Thread.sleep(2000L)
         }
-        .timeout(1000L,TimeUnit.MILLISECONDS)
+        .timeout(1000L, TimeUnit.MILLISECONDS)
         .doOnNext {
             println("No Timeout")
         }
-        .doOnError { throw it }
-        .subscribe()
-    Thread.sleep(5000L)
+        .doOnError {
+            if (it is TimeoutException) {
+                println("No Timeout")
+            }
+        }
+        .blockingSubscribe()
 }
