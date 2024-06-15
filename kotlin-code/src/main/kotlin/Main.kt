131984127c9db74d4b18e6d002601b441ffd7229
easing <easing@onyx-international.com>
2024-06-15 15:06:58 +0800

    createSingleThreadScheduler

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
-fun main(){
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
 
+fun main() {
+
+}
+
+fun createSingleThreadScheduler(name: String): Scheduler {
+    val threadFactory = ThreadFactory { runnable ->
+        val thread = Thread(runnable)
+        thread.name = name
+        return@ThreadFactory thread
+    }
+    val executor = Executors.newSingleThreadScheduledExecutor(threadFactory)
+    return Schedulers.from(executor)
 }
