43f0941a70db8333d2bfaf0caced2a8a2b55873f
easing <easing@onyx-international.com>
2024-06-15 16:43:34 +0800

    rxjava timeout

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
-import io.reactivex.rxjava3.core.Scheduler
-import io.reactivex.rxjava3.schedulers.Schedulers
-import java.util.concurrent.Executors
-import java.util.concurrent.ThreadFactory
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 fun main() {
-
-}
-
-fun createSingleThreadScheduler(name: String): Scheduler {
-    val threadFactory = ThreadFactory { runnable ->
-        val thread = Thread(runnable)
-        thread.name = name
-        return@ThreadFactory thread
-    }
-    val executor = Executors.newSingleThreadScheduledExecutor(threadFactory)
-    return Schedulers.from(executor)
+    Observable.just(0)
+        .map {
+            Thread.sleep(2000L)
+        }
+        .timeout(1000L,TimeUnit.MILLISECONDS)
+        .doOnNext {
+            println("No Timeout")
+        }
+        .doOnError { throw it }
+        .subscribe()
+    Thread.sleep(5000L)
 }
