7f8b937da404c88e5198a4dc877ad8618b790bba
easing <easing@onyx-international.com>
2024-06-23 15:31:24 +0800

    test des

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,15 +1,29 @@
 import java.io.FileInputStream
-import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.DESKeySpec
 
+@OptIn(ExperimentalStdlibApi::class)
 fun main() {
+    // create test data
     val file = "resources/data.txt"
     val origin = FileInputStream(file).readAllBytes()
-    // encode bytes to base64 string
-    val encoder = Base64.getEncoder()
-    val encoded = encoder.encodeToString(origin)
-    println(encoded)
-    // decode base64 string to bytes
-    val decoder = Base64.getDecoder()
-    val decoded = decoder.decode(encoded)
-    println(String(decoded))
+    // generate encrypt key
+    val generator = KeyGenerator.getInstance("DES")
+    val secretKey1 = generator.generateKey()
+    // encrypt
+    val cipher1 = Cipher.getInstance("DES")
+    cipher1.init(Cipher.ENCRYPT_MODE, secretKey1)
+    val encrypted = cipher1.doFinal(origin)
+    // generate decrypt key
+    val keySpec = DESKeySpec(secretKey1.encoded)
+    val keyFactory = SecretKeyFactory.getInstance("DES")
+    val secretKey2 = keyFactory.generateSecret(keySpec)
+    println(secretKey2 == secretKey1)
+    // decrypt
+    val cipher2 = Cipher.getInstance("DES")
+    cipher2.init(Cipher.DECRYPT_MODE, secretKey2)
+    val decrypted = cipher2.doFinal(encrypted)
+    println(decrypted.toHexString() == origin.toHexString())
 }
