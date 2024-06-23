c2df73fddeb9da944058e7247d0b0d4ce7246008
easing <easing@onyx-international.com>
2024-06-23 18:15:12 +0800

    test signature SHA256withDSA

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,24 +1,25 @@
 import java.io.FileInputStream
 import java.security.KeyPairGenerator
-import javax.crypto.Cipher
+import java.security.Signature
 
-@OptIn(ExperimentalStdlibApi::class)
 fun main() {
     val keyAlgorithm = "DSA"
-    val cipherAlgorithm = "DSA"
+    val signAlgorithm = "SHA256withDSA"
     // create test data
     val file = "resources/data.txt"
-    val origin = FileInputStream(file).readAllBytes().copyOf(245)
+    val origin = FileInputStream(file).readAllBytes()
     // generate key pair
     val keyGenerator = KeyPairGenerator.getInstance(keyAlgorithm)
     val keyPair = keyGenerator.genKeyPair()
-    // encrypt
-    val cipher1 = Cipher.getInstance(cipherAlgorithm)
-    cipher1.init(Cipher.ENCRYPT_MODE, keyPair.private)
-    val encrypted = cipher1.doFinal(origin)
-    // decrypt
-    val cipher2 = Cipher.getInstance(cipherAlgorithm)
-    cipher2.init(Cipher.DECRYPT_MODE, keyPair.public)
-    val decrypted = cipher2.doFinal(encrypted)
-    println(decrypted.toHexString() == origin.toHexString())
+    // sign
+    val sign = Signature.getInstance(signAlgorithm)
+    sign.initSign(keyPair.private)
+    sign.update(origin)
+    val signature = sign.sign()
+    // verify
+    val verify = Signature.getInstance(signAlgorithm)
+    verify.initVerify(keyPair.public)
+    verify.update(origin)
+    val result = verify.verify(signature)
+    println(result)
 }
