f059a448066807413120a28ef59fdf2c06b6c289
easing <easing@onyx-international.com>
2024-06-23 15:18:45 +0800

    test base64

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
-fun main() {
+import java.io.FileInputStream
+import java.util.Base64
 
+fun main() {
+    val file = "resources/data.txt"
+    val origin = FileInputStream(file).readAllBytes()
+    // encode bytes to base64 string
+    val encoder = Base64.getEncoder()
+    val encoded = encoder.encodeToString(origin)
+    println(encoded)
+    // decode base64 string to bytes
+    val decoder = Base64.getDecoder()
+    val decoded = decoder.decode(encoded)
+    println(String(decoded))
 }
