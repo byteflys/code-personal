8e5644d8d1376da2d82e5e8baaa0c8da950d739c
easing <easing@onyx-international.com>
2024-06-23 18:59:23 +0800

    test ecc signature

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -2,13 +2,12 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.io.FileInputStream
 import java.security.KeyPairGenerator
 import java.security.Security
-import javax.crypto.Cipher
+import java.security.Signature
 
-@OptIn(ExperimentalStdlibApi::class)
 fun main() {
     val provider = "BC"
     val keyAlgorithm = "ECIES"
-    val cipherAlgorithm = "ECIES"
+    val signAlgorithm = "SHA256withECDSA"
     // register bouncy castle provider
     Security.addProvider(BouncyCastleProvider())
     // create test data
@@ -17,13 +16,15 @@ fun main() {
     // generate key pair
     val keyGenerator = KeyPairGenerator.getInstance(keyAlgorithm, provider)
     val keyPair = keyGenerator.genKeyPair()
-    // encrypt
-    val cipher1 = Cipher.getInstance(cipherAlgorithm, provider)
-    cipher1.init(Cipher.ENCRYPT_MODE, keyPair.public)
-    val encrypted = cipher1.doFinal(origin)
-    // decrypt
-    val cipher2 = Cipher.getInstance(cipherAlgorithm, provider)
-    cipher2.init(Cipher.DECRYPT_MODE, keyPair.private)
-    val decrypted = cipher2.doFinal(encrypted)
-    println(decrypted.toHexString() == origin.toHexString())
+    // sign
+    val sign = Signature.getInstance(signAlgorithm, provider)
+    sign.initSign(keyPair.private)
+    sign.update(origin)
+    val signature = sign.sign()
+    // verify
+    val verify = Signature.getInstance(signAlgorithm, provider)
+    verify.initVerify(keyPair.public)
+    verify.update(origin)
+    val result = verify.verify(signature)
+    println(result)
 }
