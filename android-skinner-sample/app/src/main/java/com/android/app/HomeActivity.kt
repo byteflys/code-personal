da1bca025f2fe8f1bf34239c497c9c6f94b293f4
easing <easing@onyx-international.com>
2024-06-28 09:40:53 +0800

    create android skinner project

---
+++ android-skinner-sample/app/src/main/java/com/android/app/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.android.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.app.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+
+    }
+}
---
+++ android-skinner-sample/app/src/main/java/com/android/app/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.android.app
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.tencent.mmkv.MMKV
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Global.application = application
+        MMKV.initialize(application)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onResume() {
+        super.onResume()
+        if (!Environment.isExternalStorageManager()) {
+            val uri = Uri.parse("package:$packageName")
+            val intent = Intent()
+            intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+            intent.data = uri
+            intent.addCategory(Intent.CATEGORY_DEFAULT)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        } else {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+    }
+}
