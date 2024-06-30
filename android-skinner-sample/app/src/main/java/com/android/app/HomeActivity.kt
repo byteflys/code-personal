99c3e12195b72f6ec77960df72263d61b9ca9cf0
easing <easing@onyx-international.com>
2024-06-30 16:46:42 +0800

    simplify SkinnerInflaterFactory

--- android-skinner-sample/app/src/main/java/com/android/app/HomeActivity.kt
+++ android-skinner-sample/app/src/main/java/com/android/app/HomeActivity.kt
@@ -6,13 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setSkinnerInflaterFactory( this)
+        layoutInflater.factory2 = SkinnerInflaterFactory(this)
         super.onCreate(savedInstanceState)
         val root = layoutInflater.inflate(R.layout.activity_home, null)
         setContentView(root)
-    }
-
-    private fun setSkinnerInflaterFactory(  activity: AppCompatActivity) {
-        activity.layoutInflater.factory2 = SkinnerInflaterFactory(activity)
     }
 }
--- android-skinner-sample/app/src/main/java/com/android/app/SkinnerInflaterFactory.kt
+++ android-skinner-sample/app/src/main/java/com/android/app/SkinnerInflaterFactory.kt
@@ -4,19 +4,30 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.android.library.skinner.SkinnerAssetManager
+
+typealias androidStyleableRes = androidx.appcompat.R.styleable
 
 class SkinnerInflaterFactory(private val activity: AppCompatActivity) : LayoutInflater.Factory2 {
 
     override fun onCreateView(parent: View?, name: String, context: Context, attrs: AttributeSet): View? {
-        if (name == "ImageView") {
-            return TextView(context).apply { text = "Hello" }
+        val view = activity.delegate.createView(parent, name, context, attrs)
+        if (view is ImageView) {
+            skinImageView(view, attrs)
         }
-        return activity.delegate.createView(parent, name, context, attrs)
+        return view
     }
 
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return onCreateView(null, name, context, attrs)
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet) = null
+
+    private fun skinImageView(view: ImageView, attrs: AttributeSet) {
+        val typedArray = activity.obtainStyledAttributes(attrs, androidStyleableRes.AppCompatImageView)
+        if (typedArray.hasValue(androidStyleableRes.AppCompatImageView_android_src)) {
+            val srcDrawableId = typedArray.getResourceId(androidStyleableRes.AppCompatImageView_android_src, 0)
+            val skinDrawable = SkinnerAssetManager.skinDrawable(srcDrawableId)
+            view.setImageDrawable(skinDrawable)
+        }
     }
 }
