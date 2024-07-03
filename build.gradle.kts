5a70e79bc86504ed87a37e3cd932e884687fbeb8
easing <easing@onyx-international.com>
2024-07-03 08:51:11 +0800

    update gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
