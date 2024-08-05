d67ad84cafcd305d772917f72a21de68cdcbed71
easing <easing@onyx-international.com>
2024-08-06 06:45:31 +0800

    test edit focus

--- android-code/app/src/main/java/com/android/code/HomeActivity.kt
+++ android-code/app/src/main/java/com/android/code/HomeActivity.kt
@@ -1,21 +1,26 @@
 package com.android.code
 
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import com.android.code.databinding.ActivityHomeBinding
+import com.android.code.databinding.ActivityHome2Binding
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityHome2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHome2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         initView()
     }
 
     private fun initView() {
-        binding.listView.adapter = SimpleAdapter()
+        binding.button.setOnClickListener {
+            binding.editText.requestFocus()
+            val imm = getSystemService(InputMethodManager::class.java)
+            imm.showSoftInput(binding.editText, InputMethodManager.SHOW_IMPLICIT)
+        }
     }
 }
