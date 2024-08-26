a53a2726584c6e3ce4f14397c83515bc1f454ec8
easing <easing@onyx-international.com>
2024-08-26 16:02:36 +0800

    test onErrorReturn

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package com.code.kotlin
 
-fun main() {
+import io.reactivex.rxjava3.core.Observable
 
+fun main() {
+    Observable.just(Unit)
+        .doOnNext { println("next 1") }
+        .doOnError { println("error 1") }
+        .map<String> { throw RuntimeException("error") }
+        .doOnNext { println("next 2") }
+        .doOnError { println("error 2") }
+        .onErrorReturn { "error placeholder 3" }
+        .doOnNext { println("next 3") }
+        .doOnError { println("error 3") }
+        .doOnComplete { println("complete 4") }
+        .doFinally { println("finally 4") }
+        .subscribe()
 }
