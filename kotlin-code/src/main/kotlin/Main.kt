23cb9c82d71f88ec55c6014d622b844cdb4402a6
easing <easing@onyx-international.com>
2024-09-19 07:54:15 +0800

    coroutines

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -2,15 +2,20 @@ package com.code.kotlin
 
 import kotlin.coroutines.*
 
-fun main() {
-    val completeContinuation = object : Continuation<Int> {
-        override val context = EmptyCoroutineContext
-        override fun resumeWith(result: Result<Int>) {
-            println(result.getOrNull())
-        }
-    }
-    val safeContinuation = suspend {
-        100
-    }.createCoroutine(completeContinuation)
-    safeContinuation.resume(Unit)
+suspend fun main() {
+//    val completeContinuation = object : Continuation<Int> {
+//        override val context = EmptyCoroutineContext
+//        override fun resumeWith(result: Result<Int>) {
+//            println(result.getOrNull())
+//        }
+//    }
+//    val safeContinuation = suspend {
+//        100
+//    }.createCoroutine(completeContinuation)
+//    safeContinuation.resume(Unit)
+suspendCoroutine<Int> { continuation ->
+    continuation.resume(100)
+}
+
+Thread.sleep(2000L)
 }
