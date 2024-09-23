9ce3f61e1e73512aaf588bae5266349e62d18753
easing <easing@onyx-international.com>
2024-09-23 23:12:30 +0800

    coroutines async promise

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,16 +1,23 @@
 package com.code.kotlin
 
+import kotlin.concurrent.thread
+
 fun main() {
     async {
         val userName = await<String> { loadUserName() }
         println(userName)
+        println("async returned")
     }
+    println("async not return")
 }
 
 fun Promise<String>.loadUserName() {
-    try {
-        resume("hello")
-    } catch (e: Throwable) {
-        error(e)
+    thread {
+        Thread.sleep(3000L)
+        try {
+            resume("hello")
+        } catch (e: Throwable) {
+            error(e)
+        }
     }
 }
