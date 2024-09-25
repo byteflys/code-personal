890163762ba217185c9b85dadea48e09d8bfabce
easing <easing@onyx-international.com>
2024-09-25 10:03:25 +0800

    lua coroutines

--- kotlin-code/src/main/kotlin/LuaCoroutine.kt
+++ kotlin-code/src/main/kotlin/LuaCoroutine.kt
@@ -7,7 +7,8 @@ fun <PARAM, RESULT> create(
 ): LuaCoroutine<PARAM, RESULT> {
     val coroutine = LuaCoroutineImpl<PARAM, RESULT>()
     val continuation = block.createCoroutine(coroutine, coroutine)
-    coroutine.state = CoroutineState.WAITING(continuation)
+    coroutine.state = LuaCoroutineState.CREATED(continuation)
+    continuation.resume(Unit)
     return coroutine
 }
 
@@ -20,10 +21,11 @@ interface LuaCoroutineScope<PARAM, RESULT> {
     suspend fun yield(value: RESULT): PARAM
 }
 
-internal sealed class CoroutineState<RESULT> {
-    class WAITING<RESULT>(val continuation: Continuation<Unit>) : CoroutineState<RESULT>()
-    class READY<RESULT>(val continuation: Continuation<Unit>, val value: RESULT) : CoroutineState<RESULT>()
-    class COMPLETED<RESULT> : CoroutineState<RESULT>()
+internal sealed class LuaCoroutineState<PARAM, RESULT> {
+    class CREATED<PARAM, RESULT>(val continuation: Continuation<Unit>) : LuaCoroutineState<PARAM, RESULT>()
+    class SUSPENDED<PARAM, RESULT>(val continuation: Continuation<PARAM>, val value: RESULT) : LuaCoroutineState<PARAM, RESULT>()
+    class RESUMED<PARAM, RESULT>(val value: PARAM) : LuaCoroutineState<PARAM, RESULT>()
+    class COMPLETED<PARAM, RESULT> : LuaCoroutineState<PARAM, RESULT>()
 }
 
 internal class LuaCoroutineImpl<PARAM, RESULT> :
@@ -31,14 +33,29 @@ internal class LuaCoroutineImpl<PARAM, RESULT> :
 
     override val context = EmptyCoroutineContext
 
-    internal lateinit var state: CoroutineState<RESULT>
+    internal lateinit var state: LuaCoroutineState<PARAM, RESULT>
 
-    override suspend fun yield(value: RESULT): PARAM {
-        TODO("Not yet implemented")
+    override suspend fun yield(value: RESULT): PARAM = suspendCoroutine { continuation ->
+        when (val old = state) {
+            is LuaCoroutineState.CREATED<*, *>,
+            is LuaCoroutineState.RESUMED<*, *> -> {
+                state = LuaCoroutineState.SUSPENDED(continuation, value)
+            }
+            is LuaCoroutineState.SUSPENDED<*, *>,
+            is LuaCoroutineState.COMPLETED<*, *> -> throw IllegalStateException()
+        }
     }
 
     override fun resume(value: PARAM): RESULT {
-        TODO("Not yet implemented")
+        when (val old = state) {
+            is LuaCoroutineState.SUSPENDED<PARAM, RESULT> -> {
+                state = LuaCoroutineState.RESUMED(value)
+                return old.value
+            }
+            is LuaCoroutineState.CREATED<PARAM, RESULT>,
+            is LuaCoroutineState.RESUMED<PARAM, RESULT>,
+            is LuaCoroutineState.COMPLETED<PARAM, RESULT> -> throw IllegalStateException()
+        }
     }
 
     override fun resumeWith(result: Result<Unit>) {
