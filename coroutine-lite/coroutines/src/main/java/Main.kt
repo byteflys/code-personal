9cd7cf61157dd012647e46941c50fea91d6084d2
easing <easing@onyx-international.com>
2024-10-13 18:59:36 +0800

    coroutine lite

--- coroutine-lite/coroutines/src/main/java/Main.kt
+++ coroutine-lite/coroutines/src/main/java/Main.kt
@@ -1,17 +1,31 @@
+import com.bennyhuo.kotlin.coroutines.cancel.suspendCancellableCoroutine
 import com.bennyhuo.kotlin.coroutines.delay
-import com.bennyhuo.kotlin.coroutines.dispatcher.Dispatchers
 import com.bennyhuo.kotlin.coroutines.launch
 import com.bennyhuo.kotlin.coroutines.runBlocking
+import java.util.concurrent.CompletableFuture
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 suspend fun main() {
     runBlocking {
-        val job = launch {
-            println(1)
-            delay(1000)
-            println(2)
+        launch {
+            val result = suspendCancellableCoroutine<Int> { continuation ->
+                val future = CompletableFuture.supplyAsync {
+                    Thread.sleep(3000L)
+                    return@supplyAsync 100
+                }
+                future.thenApply {
+                    continuation.resume(it)
+                }
+                continuation.invokeOnCancellation {
+                    future.cancel(true)
+                    continuation.resume(-1)
+                }
+                println("1")
+                Thread.sleep(1000L)
+                continuation.cancel()
+            }
+            println(result)
         }
-        println(3)
-        job.join()
-        println(4)
     }
 }
