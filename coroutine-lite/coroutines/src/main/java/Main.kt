22c292e9a9096623b5d717d5dcf503e6dbc17933
easing <easing@onyx-international.com>
2024-10-15 10:25:32 +0800

    coroutine cancel

--- coroutine-lite/coroutines/src/main/java/Main.kt
+++ coroutine-lite/coroutines/src/main/java/Main.kt
@@ -1,10 +1,21 @@
 import com.bennyhuo.kotlin.coroutines.cancel.suspendCancellableCoroutine
 import com.bennyhuo.kotlin.coroutines.launch
 import com.bennyhuo.kotlin.coroutines.runBlocking
+import x.kotlin.commons.coroutine.Coroutines
 import java.util.concurrent.CompletableFuture
+import kotlin.concurrent.thread
 import kotlin.coroutines.resume
 
-suspend fun main() {
+fun main() {
+    thread {
+        Coroutines.startCoroutine {
+            runCoroutine()
+        }
+    }
+    Thread.sleep(5000)
+}
+
+suspend fun runCoroutine() {
     runBlocking {
         launch {
             val result = suspendCancellableCoroutine<Int> { continuation ->
@@ -13,6 +24,7 @@ suspend fun main() {
                     return@supplyAsync 100
                 }
                 future.thenApply {
+                    println("4")
                     continuation.resume(it)
                 }
                 continuation.invokeOnCancellation {
