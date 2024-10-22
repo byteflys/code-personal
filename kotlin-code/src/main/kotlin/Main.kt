be4fbd8299ebbfc82269f0ac841275bd9259c265
easing <easing@onyx-international.com>
2024-10-22 10:14:20 +0800

    kotlinx.coroutines channel produce

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
 package x.coroutine
 
-suspend fun main() {
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
 
+suspend fun main() {
+    val receiveChannel = GlobalScope.produce<Int> {
+        repeat(5) {
+            delay(300)
+            send(it)
+        }
+        close()
+        repeat(5) {
+            delay(300)
+            send(it)
+        }
+    }
+    for (element in receiveChannel) {
+        println(element)
+    }
+    println("Done!")
 }
