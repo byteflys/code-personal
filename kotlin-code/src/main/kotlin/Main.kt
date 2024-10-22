c20ee7280c6bfd1f7ff54aee18bba0a77cd67acc
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-10-22 21:11:21 +0800

    kotlinx.coroutines channel broadcast

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,21 +1,15 @@
 package x.coroutine
 
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.channels.broadcast
 import kotlinx.coroutines.delay
 
 suspend fun main() {
-    val sendChannel = GlobalScope.actor<Int> {
-        while (!isClosedForReceive) {
-            val element = receive()
-            println(element)
-        }
+   val broadcast = GlobalScope.broadcast<Int>(capacity = 5) {
+       List(3){
+
+       }
     }
-    repeat(5) {
-        sendChannel.send(it + 1)
-    }
-    sendChannel.close()
-    println(sendChannel.isClosedForSend)
     println("Done!")
     delay(9999)
 }
