a387f839ceb6b1b50762059b6c3b5cf3c31a6b66
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-10-23 22:54:00 +0800

    kotlinx.coroutines select

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,19 +1,24 @@
 package x.coroutine
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.selects.select
 
 suspend fun main() {
-    flow<Int> {
-        repeat(5) {
-            delay(300)
-            emit(it + 1)
-        }
-    }.onEach {
-        println(it)
-    }.collect()
+    val deferred1 = GlobalScope.async {
+        delay(2000)
+        return@async 1
+    }
+    val deferred2 = GlobalScope.async {
+        delay(1000)
+        return@async 2
+    }
+    val deferred = select<Int> {
+        deferred1.onAwait { it }
+        deferred2.onAwait { it }
+    }
+    println(deferred)
     println("Done!")
     delay(9999)
 }
