338f69a47309fe240b6a72c0e687b06f7d97268e
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-10-28 00:08:42 +0800

    kotlinx.coroutines exception

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,14 +1,26 @@
 package x.coroutine
 
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 suspend fun main() {
-    val job = GlobalScope.launch {
-        delay(1000)
-        println("coroutine by launch")
+    Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+        println(throwable.message + " " + "DefaultUncaughtExceptionHandler")
     }
-    println(job.isCancelled)
+    val errorHandler = CoroutineExceptionHandler { context, throwable ->
+        println(throwable.message + " " + context[CoroutineName])
+    }
+    val job1 = GlobalScope.launch(Dispatchers.Unconfined + errorHandler + CoroutineName("1")) {
+        val job2 = launch(Dispatchers.Unconfined + errorHandler + CoroutineName("2")) {
+            delay(100)
+            throw RuntimeException("crash")
+        }
+        println("2")
+    }
+    println("1")
     delay(999 * 1000L)
 }
