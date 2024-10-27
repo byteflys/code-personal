e24f42dd992362fd56024a5ab626e54ebec66ca0
easing <easing@onyx-international.com>
2024-10-27 12:34:23 +0800

    kotlinx.coroutines launch

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -1,19 +1,20 @@
 package x.coroutine
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 suspend fun main() {
-    flow<Int> {
-        repeat(5) {
-            delay(300)
-            emit(it + 1)
-        }
-    }.onEach {
-        println(it)
-    }.collect()
-    println("Done!")
-    delay(9999)
+    val parentJob = Job()
+    val scope = CoroutineScope(parentJob)
+    val dispatcher = Dispatchers.Default
+    val start = CoroutineStart.LAZY
+    val job = scope.launch(dispatcher, start) {
+        println("coroutine by launch")
+    }
+    job.start()
+    delay(999 * 1000L)
 }
