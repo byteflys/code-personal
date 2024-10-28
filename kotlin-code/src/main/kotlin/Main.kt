27d8ce37f4f11db96bb0e91a866aa9bea7598c21
easingforever2000@gmail.com <easingforever2000@gmail.com>
2024-10-28 23:27:22 +0800

    kotlinx.coroutines exception

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package x.coroutine
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import x.kotlin.commons.coroutine.CoroutineDispatchers
@@ -11,22 +12,21 @@ import x.kotlin.commons.debug.Debug.printWithThreadInfo
 suspend fun main() {
     val dispatcher1 = CoroutineDispatchers.io()
     val dispatcher2 = CoroutineDispatchers.data()
-    Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-        printWithThreadInfo(throwable.message + " " + "DefaultUncaughtExceptionHandler")
-    }
     val errorHandler = CoroutineExceptionHandler { context, throwable ->
-        printWithThreadInfo(throwable.message + " " + context[CoroutineName])
+        printWithThreadInfo(context[CoroutineName]?.name + "-crashed")
     }
-    val job1 = GlobalScope.launch(dispatcher1 + errorHandler + CoroutineName("1")) {
-        val job2 = GlobalScope.launch(dispatcher2 + errorHandler + CoroutineName("2")) {
+    lateinit var job1: Job
+    job1 = GlobalScope.launch(dispatcher1 + errorHandler + CoroutineName("Job1")) {
+        val job2 = launch(dispatcher2 + errorHandler + CoroutineName("Job2")) {
             delay(100)
             printWithThreadInfo("1")
+            println("Job1.isCompleted = ${job1.isCompleted}")
             throw RuntimeException("crash")
         }
-        delay(200)
         printWithThreadInfo("2")
     }
-    delay(300)
+    delay(400)
     printWithThreadInfo("3")
+    println("Job1.isCompleted = ${job1.isCompleted}")
     delay(999 * 1000L)
 }
