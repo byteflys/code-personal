c098a777a979322cbaa66af15017db6f5613da6b
easing <easing@onyx-international.com>
2024-10-30 09:18:04 +0800

    kotlinx.coroutines supervisorScope

--- kotlin-code/src/main/kotlin/Main.kt
+++ kotlin-code/src/main/kotlin/Main.kt
@@ -3,31 +3,28 @@ package x.coroutine
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import x.kotlin.commons.coroutine.CoroutineDispatchers
-import x.kotlin.commons.debug.Debug.printWithThreadInfo
+import kotlinx.coroutines.supervisorScope
+import x.kotlin.commons.coroutine.XDispatchers
+import x.kotlin.commons.debug.XConsole.printWithThreadInfo
 
 suspend fun main() {
-    val dispatcher1 = CoroutineDispatchers.new()
-    val dispatcher2 = CoroutineDispatchers.data()
+    val dispatcher1 = XDispatchers.new()
+    val dispatcher2 = XDispatchers.new()
     val errorHandler = CoroutineExceptionHandler { context, throwable ->
         printWithThreadInfo(context[CoroutineName]?.name + "-crashed")
     }
-    lateinit var job2: Job
     val job1 = GlobalScope.launch(dispatcher1 + errorHandler + CoroutineName("Job1")) {
-          job2 = launch(dispatcher2 + errorHandler + CoroutineName("Job2")) {
-            delay(200)
+        val job2 = launch(dispatcher2 + errorHandler + CoroutineName("Job2")) {
+            delay(100)
             printWithThreadInfo("Job2")
             throw RuntimeException("crash")
         }
-        delay(400)
+        delay(200)
         printWithThreadInfo("Job1")
     }
-    delay(600)
-    job1.join()
-    job2.join()
+    delay(400)
     printWithThreadInfo("Main")
     delay(1 * 1000L)
 }
